e6017137608af523ee28426541d802d328d1ab30
Marc Nijdam <men@Marcs-MacBook-Pro.local>
2019-11-25 19:05:03 -0700

    Simplify to pick up top level txn wrapper

--- build.rs
+++ build.rs
@@ -1,22 +1,9 @@
 use prost_build;
-use std::{
-    fs::{self},
-    io::Result,
-    path::Path,
-};
-
-fn file_names(path: &str) -> Result<Vec<String>> {
-    let read_dir = fs::read_dir(Path::new(path))?;
-    let mut names = Vec::new();
-    for dir_entry in read_dir {
-        let file_name = dir_entry?.path().to_string_lossy().into_owned();
-        names.push(file_name);
-    }
-    Ok(names)
-}
+use std::io::Result;
 
 fn main() -> Result<()> {
-    let txn_v1_names = file_names("src/blockchain/transactions/v1")?;
-    prost_build::compile_protos(&txn_v1_names, &["src".to_string()]).unwrap();
+    let txn_proto = ["src/blockchain/transactions/txn.proto"];
+    prost_build::compile_protos(&txn_proto, &["src/blockchain/transactions"]).unwrap();
+
     Ok(())
 }
