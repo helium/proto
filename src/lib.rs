86baf131b4930c6764bced447b49e7066021a093
Marc Nijdam <marc_nijdam@mac.com>
2019-11-27 08:39:10 -0700

    Simplify module depth

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub mod txns {
-    pub mod blockchain {
-        include!(concat!(env!("OUT_DIR"), "/helium.txns.rs"));
-    }
+pub mod txn {
+    include!(concat!(env!("OUT_DIR"), "/helium.txns.rs"));
+    pub use txn::Txn as Wrapper;
 }
+
+pub use prost::Message;
