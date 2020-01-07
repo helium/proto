b358a0e9fb7d30ea61369f86c28178a2f2573864
Louis Thiery <louis@helium.com>
2020-01-06 17:10:33 -0800

    fix rust crate

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ use prost_build;
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let txn_proto = ["src/blockchain/transactions/txn.proto"];
-    prost_build::compile_protos(&txn_proto, &["src/blockchain/transactions"]).unwrap();
+    let txn_proto = ["src/blockchain_txn.proto"];
+    prost_build::compile_protos(&txn_proto, &["src/"]).unwrap();
 
     Ok(())
 }
