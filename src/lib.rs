54b7a5931f9ab779f21385a92c28e15313ebb1fd
Marc Nijdam <marc@helium.com>
2020-02-10 12:05:59 -0700

    Fix up protos to pass Rust builds (#16)
    * Fix up crate build.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-pub mod txn {
+pub mod blockchain {
     include!(concat!(env!("OUT_DIR"), "/helium.txns.rs"));
-    pub use txn::Txn as Wrapper;
+    pub use blockchain_txn::Txn;
+}
+
+pub mod packet {
+    include!(concat!(env!("OUT_DIR"), "/helium.packet.rs"));
 }
 
 pub use prost::Message;
