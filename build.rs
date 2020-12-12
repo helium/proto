999c44a1ef76d82e712b4386989ffe2aaa9d333a
Marc Nijdam <marc_nijdam@mac.com>
2020-12-12 16:02:56 -0600

    add router and validator grpc services

--- build.rs
+++ build.rs
@@ -1,9 +1,25 @@
-use prost_build;
 use std::io::Result;
 
+#[cfg(feature = "services")]
 fn main() -> Result<()> {
-    let txn_proto = ["src/blockchain_txn.proto"];
-    prost_build::compile_protos(&txn_proto, &["src/"]).unwrap();
+    tonic_build::configure()
+        .build_server(false)
+        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
+        .compile(
+            &[
+                "src/blockchain_txn.proto",
+                "src/service/router.proto",
+                "src/service/validator.proto",
+            ],
+            &["src/"],
+        )?;
+    Ok(())
+}
 
+#[cfg(not(feature = "services"))]
+fn main() -> Result<()> {
+    prost_build::Config::new()
+        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
+        .compile_protos(&["src/blockchain_txn.proto"], &["src/"])?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 include!(concat!(env!("OUT_DIR"), "/helium.rs"));
 pub use blockchain_txn::Txn;
 pub use prost::Message;
+#[cfg(feature = "services")]
+pub mod services {
+    pub mod router {
+        pub use crate::router_client::RouterClient as Client;
+    }
+    pub mod validator {
+        pub use crate::validator_client::ValidatorClient as Client;
+    }
+
+    pub use tonic::transport::*;
+}
