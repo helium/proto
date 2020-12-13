2ff0f17a96209e220af0d5f6b333a9faf623cb64
Louis Thiery <thiery.louis@gmail.com>
2020-12-12 16:21:02 -0800

    compartmentalize Rust

--- build.rs
+++
@@ -1,9 +0,0 @@
-use prost_build;
-use std::io::Result;
-
-fn main() -> Result<()> {
-    let txn_proto = ["src/blockchain_txn.proto"];
-    prost_build::compile_protos(&txn_proto, &["src/"]).unwrap();
-
-    Ok(())
-}
