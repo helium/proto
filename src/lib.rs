cb7d0d514c2ae101c9c931ef1d31f09fefdbc20d
Marc Nijdam <marc_nijdam@mac.com>
2021-11-04 07:36:05 -0700

    Add local module for services

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 include!(concat!(env!("OUT_DIR"), "/helium.rs"));
+
+#[cfg(feature = "services")]
+include!(concat!(env!("OUT_DIR"), "/helium.local.rs"));
+
 pub use blockchain_txn::Txn;
 pub use prost::{DecodeError, EncodeError, Message};
 
@@ -10,6 +14,11 @@ pub mod services {
     }
     pub mod gateway {
         pub use crate::gateway_client::GatewayClient as Client;
+    }
+
+    pub mod local {
+        pub use crate::api_client::ApiClient as Client;
+        pub use crate::api_server::ApiServer as Server;
     }
 
     pub use tonic::transport::*;
