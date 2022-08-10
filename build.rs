c68731ec6287a36ec780f38f9fcfb42fe35c6732
jeffgrunewald <jeff.grunewald@gmail.com>
2022-08-10 18:58:30 -0400

    include txn proto in rust builds

--- build.rs
+++ build.rs
@@ -12,6 +12,7 @@ fn main() -> Result<()> {
                 "src/service/state_channel.proto",
                 "src/service/gateway.proto",
                 "src/service/follower.proto",
+                "src/service/transaction.proto",
             ],
             &["src/"],
         )?;
