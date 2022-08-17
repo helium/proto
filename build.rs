0f8095f89a8bbb40dd2dcd280a4b90276cbbfaab
Jay Kickliter <jay@kickliter.com>
2022-08-17 11:38:34 -0600

    Do not use protobuf-src for Windows builds

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 use std::io::Result;
 #[cfg(feature = "services")]
 fn main() -> Result<()> {
+    #[cfg(target_family = "windows")]
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    #[cfg(not(target_family = "windows"))]
     std::env::set_var("PROTOC", protobuf_src::protoc());
+
     tonic_build::configure()
         .build_server(false)
         .type_attribute(".", "#[derive(serde_derive::Serialize)]")
@@ -29,6 +33,9 @@ fn main() -> Result<()> {
 
 #[cfg(not(feature = "services"))]
 fn main() -> Result<()> {
+    #[cfg(target_family = "windows")]
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    #[cfg(not(target_family = "windows"))]
     std::env::set_var("PROTOC", protobuf_src::protoc());
     prost_build::Config::new()
         .type_attribute(".", "#[derive(serde_derive::Serialize)]")
