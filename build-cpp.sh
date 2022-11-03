16646a75c98d06c55a07ec12c318f913075d1b49
Vladyslav Zhuk <malcolm.madsheep@gmail.com>
2022-11-03 19:04:50 +0200

    add CPP build workflow (#215)
    * add CPP build workflow
    
    * update build-cpp.sh

---
+++ build-cpp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SERVICES=$(echo src/service/{config,follower,gateway,local,packet_router,poc_lora,poc_mobile,router,state_channel,transaction}.proto)
+MESSAGES=$(echo src/{blockchain_txn,entropy,data_rate,region,mapper}.proto)
+SRC_FILES="$SERVICES $MESSAGES"
+
+protoc -I=src --cpp_out=. $SRC_FILES
