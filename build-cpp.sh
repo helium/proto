138dd490894a17c973370d6ecabddfe06a35dcff
Vladyslav Zhuk <vzhuk@freedomfi.com>
2022-11-03 18:03:20 +0200

    update build-cpp.sh

--- build-cpp.sh
+++ build-cpp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SERVICES=$(echo src/service/{attach_response,follower,gateway,gps_response,local,modem_response,packet_router,poc_lora,poc_mobile,router,scan_response,state_channel,transaction}.proto)
-MESSAGES=$(echo src/{blockchain_txn,entropy,data_rate,region}.proto)
+SERVICES=$(echo src/service/{config,follower,gateway,local,packet_router,poc_lora,poc_mobile,router,state_channel,transaction}.proto)
+MESSAGES=$(echo src/{blockchain_txn,entropy,data_rate,region,mapper}.proto)
 SRC_FILES="$SERVICES $MESSAGES"
 
 protoc -I=src --cpp_out=. $SRC_FILES
