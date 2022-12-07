87d1d23b38e6ab980b16bbcbdb38126727aac268
Macpie <pierre@nova-labs.com>
2022-12-06 16:00:01 -0800

    More fixes

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ pub mod services {
 
     pub mod downlink {
         include!(concat!(env!("OUT_DIR"), "/helium.downlink.rs"));
-        pub use http_roaming_server::{Downlink, DownlinkServer as Server};
+        pub use http_roaming_server::{HttpRoaming, HttpRoamingServer as Server};
     }
 
     pub mod router {
