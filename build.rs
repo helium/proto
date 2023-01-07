c51214dbd1e9ac6e6b6ced56a2dd8a604a7273be
Jeff Grunewald <jeff.grunewald@gmail.com>
2023-01-07 14:40:07 -0500

    derive deser for region (and others) (#259)
    * derive deser for region (and others)
    
    * remove previous config attribute

--- build.rs
+++ build.rs
@@ -32,8 +32,10 @@ fn main() -> Result<()> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
-        .type_attribute(".helium.config", "#[derive(serde_derive::Deserialize)]")
+        .type_attribute(
+            ".",
+            "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]",
+        )
         .compile(
             &MESSAGES
                 .iter()
@@ -49,7 +51,10 @@ fn main() -> Result<()> {
 fn main() -> Result<()> {
     std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
     prost_build::Config::new()
-        .type_attribute(".", "#[derive(serde_derive::Serialize)]")
+        .type_attribute(
+            ".",
+            "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]",
+        )
         .compile_protos(MESSAGES, &["src"])?;
     Ok(())
 }
