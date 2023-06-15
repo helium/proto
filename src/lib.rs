e842ce1273de919d7b643c5a3616094157045404
jeffgrunewald <jeff.grunewald@gmail.com>
2023-06-15 09:08:50 -0400

    simplify subscriber verification

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,14 @@ pub mod services {
 
     pub mod mobile_config {
         include!(concat!(env!("OUT_DIR"), "/helium.mobile_config.rs"));
+        pub use admin_client as config_admin_client;
         pub use admin_server::{Admin, AdminServer};
         pub use authorization_client::AuthorizationClient;
         pub use authorization_server::{Authorization, AuthorizationServer};
+        pub use entity_client::EntityClient;
+        pub use entity_server::{Entity, EntityServer};
         pub use gateway_client::GatewayClient;
         pub use gateway_server::{Gateway, GatewayServer};
-        pub use subscriber_client::SubscriberClient;
-        pub use subscriber_server::{Subscriber, SubscriberServer};
     }
 
     pub mod downlink {
