4824d15f6c884c7ab97d5c336edccd721389e9c7
Connor McKenzie <connora.mckenzie@gmail.com>
2025-11-16 23:30:36 +0000

    Removed MsgVerify

--- msg-signature/src/lib.rs
+++ msg-signature/src/lib.rs
@@ -1,18 +1,4 @@
-// use helium_crypto::PublicKey;
 pub use msg_verify_macro::MsgHasSignature;
-
-// #[derive(thiserror::Error, Debug)]
-// pub enum MsgVerifyError {
-//     #[error("prost encode error: {0}")]
-//     Prost(#[from] prost::EncodeError),
-//
-//     #[error("crypto error: {0}")]
-//     Crypto(#[from] helium_crypto::Error),
-// }
-//
-// pub trait MsgVerify {
-//     fn verify(&self, verifier: &PublicKey) -> Result<(), MsgVerifyError>;
-// }
 
 pub trait MsgHasSignature {
     fn signature(&self) -> &[u8];
