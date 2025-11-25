55d076d4de822a42688a6b648d7f8db45d20d749
Connor McKenzie <connora.mckenzie@gmail.com>
2025-11-25 12:29:54 +0000

    Take ownership of signature in set_signature

--- msg-signature/src/lib.rs
+++ msg-signature/src/lib.rs
@@ -3,6 +3,6 @@ pub use msg_signature_macro::MsgHasSignature;
 pub trait MsgHasSignature {
     fn signature(&self) -> &[u8];
     fn without_signature(&self) -> Self;
-    fn set_signature(&mut self, signature: &[u8]);
+    fn set_signature(&mut self, signature: [u8]);
     fn clear_signature(&mut self);
 }
